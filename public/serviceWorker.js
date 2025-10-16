1ac01d581d77931602751d0e6f08d71c3382bcc9
andreylson12 <Andreylsonvirtual@gmail.com>
2025-10-16 16:32:04 -0300

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,24 +1,6 @@
+
 const CACHE_NAME = "controle-sementes-v1";
 const ASSETS = ["/","/index.html","/styles.css","/script.js","/manifest.json"];
-
-self.addEventListener("install",(e)=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate",(e)=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-});
-
-self.addEventListener("fetch",(e)=>{
-  e.respondWith(
-    caches.match(e.request).then(cached=>{
-      return cached || fetch(e.request).then(resp=>{
-        if (e.request.method==="GET" && resp.ok) {
-          const clone = resp.clone();
-          caches.open(CACHE_NAME).then(c=>c.put(e.request, clone));
-        }
-        return resp;
-      }).catch(()=>caches.match("/"));
-    })
-  );
-});
+self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});
+self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(e.request.method==="GET"&&r.ok){const x=r.clone();caches.open(CACHE_NAME).then(C=>C.put(e.request,x));}return r;}).catch(()=>caches.match("/"))))});
